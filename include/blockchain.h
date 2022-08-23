e312120b79b991274345450688311d2af29fc9be
WallabyG <ycm1366@gmail.com>
2022-08-24 00:52:40 +0900

    Modify function signatures

--- include/blockchain.h
+++ include/blockchain.h
@@ -2,8 +2,8 @@
 
 #include "blockchain_type.h"
 
-Block generate_genesis_block(Transactions_t transactions);
+Block generate_genesis_block(Transactions_t &transactions);
 
-Block generate_next_block(Block &prev_block, Transactions_t transactions);
+Block generate_next_block(Block &prev_block, Transactions_t &transactions);
 
 bool verify_blocks(Block target);
--- src/blockchain.cpp
+++ src/blockchain.cpp
@@ -3,13 +3,18 @@
 #include "cryptopp/cryptlib.h"
 #include "cryptopp/sha3.h"
 
-Block generate_genesis_block(Transactions_t transactions)
+Block generate_genesis_block(Transactions_t &transactions)
 {
     uint160_t u0 = 0;
     return Block(u0, transactions);
 }
 
-Block generate_next_block(Block &prev_block, Transactions_t transactions) {
+Block generate_next_block(Block &prev_block, Transactions_t &transactions) {
     uint160_t prev_hash = prev_block.hash_block();
     return Block(prev_hash, transactions);
+}
+
+bool verify_blocks(Block target)
+{
+    return false;
 }
