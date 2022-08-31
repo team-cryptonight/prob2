abe7cc2debc39ac35fc0d1f9edfa33e01bf4d80e
WallabyG <ycm1366@gmail.com>
2022-08-31 20:00:18 +0900

    Modify test code

--- src/main.cpp
+++ src/main.cpp
@@ -51,11 +51,20 @@ int main()
     transactions = generate_random_transactions(transactions, gen);
     Block genesis_block = generate_genesis_block(transactions, prover);
 
+    std::cout << "Block id of genesis_block:" << genesis_block.block_id << std::endl;
+    std::cout << "Hash value of previous block: " << genesis_block.hash_value << std::endl;
+
     transactions = generate_random_transactions(transactions, gen);
     Block first_block = generate_next_block(genesis_block, transactions, prover);
 
+    std::cout << "Block id of first_block:" << first_block.block_id << std::endl;
+    std::cout << "Hash value of previous block: " << first_block.hash_value << std::endl;
+
     transactions = generate_random_transactions(transactions, gen);
     Block second_block = generate_next_block(first_block, transactions, prover);
+
+    std::cout << "Block id of second_block:" << second_block.block_id << std::endl;
+    std::cout << "Hash value of previous block: " << second_block.hash_value << std::endl;
 
     transactions = generate_random_transactions(transactions, gen);
     Block third_block = generate_next_block(second_block, transactions, prover);
@@ -66,7 +75,9 @@ int main()
     transactions_to_verify.push_back(first_block.transactions[127]);
     transactions_to_verify.push_back(first_block.transactions[1023]);
 
-    std::cout << verify_transactions(transactions_to_verify, first_block, prover) << std::endl;
+    std::cout << "These transactions are " 
+        << (verify_transactions(transactions_to_verify, first_block, prover) ? "valid" : "invalid")
+        << std::endl;
 
     transactions_to_verify.clear();
 
@@ -74,7 +85,9 @@ int main()
     transactions_to_verify.push_back(second_block.transactions[255]);
     transactions_to_verify.push_back(second_block.transactions[2047]);
 
-    std::cout << verify_transactions(transactions_to_verify, second_block, prover) << std::endl;
+    std::cout << "These transactions are " 
+        << (verify_transactions(transactions_to_verify, second_block, prover) ? "valid" : "invalid")
+        << std::endl;
 
     return 0;
 }
